9672b9e4dd1c719e84f94c6e46c9ce35353b23a7
David Nicolson <david.nicolson@gmail.com>
2018-07-10 22:22:19 +0200

    Set Dock order

--- setup.sh
+++ setup.sh
@@ -21,3 +21,27 @@ qlmanage -r
 
 chflags nohidden ~/Library/
 
+dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/Applications/iTunes.app"
+dockutil --no-restart --add "/Applications/Messages.app"
+dockutil --no-restart --add "/Applications/Airmail 3.app"
+dockutil --no-restart --add "/Applications/VLC.app"
+dockutil --no-restart --add "/Applications/Photos.app"
+dockutil --no-restart --add "/Applications/ReadKit.app"
+dockutil --no-restart --add "/Applications/Wunderlist.app"
+dockutil --no-restart --add "/Applications/Notes.app"
+dockutil --no-restart --add "/Applications/1Password.app"
+dockutil --no-restart --add "/Applications/Outbank.app"
+dockutil --no-restart --add "/Applications/Google Chrome.app"
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Tower.app"
+dockutil --no-restart --add "/Applications/Sublime Text.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Sequel Pro.app"
+dockutil --no-restart --add "/Applications/0xED.app"
+dockutil --no-restart --add "/Applications/Utilities/Console.app"
+dockutil --no-restart --add "/Applications/Utilities/Activity Monitor.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
+
+killall Dock
