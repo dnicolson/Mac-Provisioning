b2f7412b3f2e86528a8005816e69ed65f15182bb
David Nicolson <david.nicolson@gmail.com>
2018-07-30 14:10:01 +0200

    Add login items

--- defaults.sh
+++ defaults.sh
@@ -11,3 +11,10 @@ defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
 # Kill iTunes Helper
 osascript -e 'tell application "System Events" to delete login item "iTunesHelper"'
+
+# Add login items
+osascript -e 'tell application "System Events" to make login item at end with properties {name: "AppTrap", path: (system attribute "HOME") & "/Library/PreferencePanes/AppTrap.prefPane/Contents/Resources/AppTrap.app"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {name: "Alfred 3", path: "/Applications/Alfred 3.app"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {name: "Itsycal", path: "/Applications/Itsycal.app"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {name: "HighSierraMediaKeyEnabler", path: "/Applications/HighSierraMediaKeyEnabler.app"}'
+osascript -e 'tell application "System Events" to make login item at end with properties {name: "Rocket", path: "/Applications/Rocket.app"}'
