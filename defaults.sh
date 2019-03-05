b9ca0ba6d0978cf0b6a76ebc9a7bb837741fa5d8
David Nicolson <david.nicolson@gmail.com>
2019-03-05 20:17:01 +0100

    Replace AppTrap with AppCleaner

--- defaults.sh
+++ defaults.sh
@@ -20,7 +20,6 @@ osascript -e 'tell application "System Events" to delete login item "iTunesHelpe
 
 # Add login items
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "Alfred 3", path: "/Applications/Alfred 3.app"}'
-osascript -e 'tell application "System Events" to make login item at end with properties {name: "AppTrap", path: (system attribute "HOME") & "/Library/PreferencePanes/AppTrap.prefPane/Contents/Resources/AppTrap.app"}'
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "Itsycal", path: "/Applications/Itsycal.app"}'
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "MacMediaKeyForwarder", path: "/Applications/MacMediaKeyForwarder.app"}'
 osascript -e 'tell application "System Events" to make login item at end with properties {name: "PixelSnap", path: "/Applications/PixelSnap.app"}'
