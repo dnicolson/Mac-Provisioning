f0cba0ec0a75ac91f20bf80494a597e4ad4d5340
Dave Nicolson <david.nicolson@gmail.com>
2020-01-19 18:07:51 +0100

    Add move to trash Dropbox warning

--- defaults/dropbox.sh
+++ defaults/dropbox.sh
@@ -16,16 +16,19 @@ gcc -o sqlite3 -I. -DSQLITE_HAS_CODEC -DHAVE_READLINE shell.c sqlite3.c -Wall -g
 # - Disable camera uploads
 # - Remove the Chrome extension notification
 # - Disable the move out of Dropbox warning
+# - Disable the move to trash warning
 
 ./sqlite3 -key $KEY ~/.dropbox/instance1/config.dbx <<END
 INSERT INTO config (key, value) VALUES ('save_screenshots', 0);
 INSERT INTO config (key, value) VALUES ('photo_import', 0);
 INSERT INTO config (key, value) VALUES ('chrome-extension-notif-seen', 1);
 INSERT INTO config (key, value) VALUES ('RestorationRule', X'80027D7100580500000072756C6532710188732E');
+INSERT INTO config (key, value) VALUES ('FSWSuppressionSettings', X'80027D710028580E000000726573746F726174696F6E735F327101885821000000726573746F726174696F6E735F315F616E645F335F696E61636365737369626C65710288752E');
 UPDATE config SET value=0 WHERE key='save_screenshots';
 UPDATE config SET value=0 WHERE key='photo_import';
 UPDATE config SET value=1 WHERE key='chrome-extension-notif-seen';
 UPDATE config SET value=X'80027D7100580500000072756C6532710188732E' WHERE key='RestorationRule';
+UPDATE config SET value=X'80027D710028580E000000726573746F726174696F6E735F327101885821000000726573746F726174696F6E735F315F616E645F335F696E61636365737369626C65710288752E' WHERE key='FSWSuppressionSettings';
 END
 
 cd ..
