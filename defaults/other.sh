20355ca483a4c8fe3c00838e3d8466c015edc1d9
Dave Nicolson <david.nicolson@gmail.com>
2020-12-02 00:14:05 +0100

    Fix TickTick defaults

--- defaults/other.sh
+++ defaults/other.sh
@@ -16,7 +16,28 @@ read -p "ℹ️  Setup Chrome and close application..."
 # TickTick
 open -a "TickTick"
 read -p "ℹ️  Setup TickTick and close application..."
-plutil -replace TKQuickAddTaskHotkeyIdentifier -json '{}' ~/Library/Group\ Containers/75TY9UT8AY.com.TickTick.task.mac/Library/Preferences/75TY9UT8AY.com.TickTick.task.mac.plist
+
+# Remove the Shift-Command-A hot key
+defaults write com.TickTick.task.mac TKQuickAddTaskHotkeyIdentifier -data ""
+
+# Hide Smart Lists
+read -r -d '' XML <<'EOF'
+<dict>
+  <key>5fc03fcf9b4251d53f94d30e</key>
+  <dict>
+    <key>Smart_CALENDAR_PROJECT_ID</key>
+    <integer>1</integer>
+    <key>Smart_NEXT7DAYS_PROJECT_ID</key>
+    <integer>1</integer>
+    <key>Smart_TODAY_PROJECT_ID</key>
+    <integer>1</integer>
+    <key>Smart_TOMORROW_PROJECT_ID</key>
+    <integer>1</integer>
+  </dict>
+</dict>
+EOF
+plutil -remove smart_project_display_status ~/Library/Group\ Containers/75TY9UT8AY.com.TickTick.task.mac/Library/Preferences/75TY9UT8AY.com.TickTick.task.mac.plist
+plutil -insert smart_project_display_status -xml "$XML" ~/Library/Group\ Containers/75TY9UT8AY.com.TickTick.task.mac/Library/Preferences/75TY9UT8AY.com.TickTick.task.mac.plist
 
 # Calendar
 open -a "Calendar"
