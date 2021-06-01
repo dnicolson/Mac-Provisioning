cd7045d01a163f0ef64acaa3874627fd4bbaaa00
Dave Nicolson <david.nicolson@gmail.com>
2021-06-01 19:27:37 +0200

    Disable Dropbox backup notification

--- defaults/dropbox.sh
+++ defaults/dropbox.sh
@@ -18,6 +18,7 @@ gcc -o sqlite3 -I. -DSQLITE_HAS_CODEC -DHAVE_READLINE shell.c sqlite3.c -Wall -g
 # - Disable the move out of Dropbox warning
 # - Disable the move to trash warning
 # - Disable the get notified about important activity notification
+# - Disable "Keep your Mac files backed up." notification
 
 ./sqlite3 -key $KEY ~/.dropbox/instance1/config.dbx <<END
 INSERT OR IGNORE INTO config (key, value) VALUES ('save_screenshots', 0);
@@ -26,12 +27,14 @@ INSERT OR IGNORE INTO config (key, value) VALUES ('chrome-extension-notif-seen',
 INSERT OR IGNORE INTO config (key, value) VALUES ('RestorationRule', X'80027D7100580500000072756C6532710188732E');
 INSERT OR IGNORE INTO config (key, value) VALUES ('FSWSuppressionSettings', X'80027D710028580E000000726573746F726174696F6E735F327101885821000000726573746F726174696F6E735F315F616E645F335F696E61636365737369626C65710288752E');
 INSERT OR IGNORE INTO config (key, value) VALUES ('mac_notifications_authorization_sticky_dismissed', 1);
+INSERT OR IGNORE INTO config (key, value) VALUES ('desktop-sync-everything-sticky-dismissed', 1);
 UPDATE config SET value=0 WHERE key='save_screenshots';
 UPDATE config SET value=0 WHERE key='photo_import';
 UPDATE config SET value=1 WHERE key='chrome-extension-notif-seen';
 UPDATE config SET value=X'80027D7100580500000072756C6532710188732E' WHERE key='RestorationRule';
 UPDATE config SET value=X'80027D710028580E000000726573746F726174696F6E735F327101885821000000726573746F726174696F6E735F315F616E645F335F696E61636365737369626C65710288752E' WHERE key='FSWSuppressionSettings';
 UPDATE config SET value=1 WHERE key='mac_notifications_authorization_sticky_dismissed';
+UPDATE config SET value=1 WHERE key='desktop-sync-everything-sticky-dismissed';
 END
 
 cd ..
