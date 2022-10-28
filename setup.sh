b9092342624edd594fb6d8bc58d4c5fa5fea24d1
Dave Nicolson <david.nicolson@gmail.com>
2022-10-28 17:18:50 +0200

    Update Homebrew install script

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ xcode-select --install
 
 # Homebrew
 if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Log in to the App Store
