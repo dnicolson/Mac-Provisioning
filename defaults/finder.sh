33ccce1065fa0ac47fb5952866d5b3cb565a0a8f
Dave Nicolson <david.nicolson@gmail.com>
2024-01-18 21:04:31 +0100

    Simplify Finder toolbar

--- defaults/finder.sh
+++ defaults/finder.sh
@@ -1,26 +1,21 @@
 #!/usr/bin/env bash
 
 PLIST=~/Library/Preferences/com.apple.finder.plist
-ITEMS=`/usr/libexec/PlistBuddy -c "Print 'NSToolbar Configuration Browser:TB Default Item Identifiers'" $PLIST | sed '1d; $d'`
-APP=/Applications/Go2Shell.app/Contents/MacOS/Go2ShellHelper.app/
-POSITION=8
+APP=/Applications/Go2Shell.app/Contents/MacOS/Go2ShellHelper.app
 
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration Browser:TB Item Identifiers'" $PLIST
-/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration Browser:TB Item Plists'" $PLIST 2>/dev/null
+/usr/libexec/PlistBuddy -c "Delete 'NSToolbar Configuration Browser':'TB Item Identifiers'" $PLIST
 
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Identifiers' array" $PLIST
-i=1
-for ITEM in $ITEMS
-do
-  if [ $i -ne $POSITION ]; then
-    /usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Identifiers:$i' string $ITEM" $PLIST
-  fi
-  ((i++))
-done
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers' array" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':0 string com.apple.finder.BACK" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':1 string NSToolbarFlexibleSpaceItem" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':2 string com.apple.finder.SWCH" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':3 string com.apple.finder.ARNG" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':4 string com.apple.finder.NFLD" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':5 string NSToolbarFlexibleSpaceItem" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser':'TB Item Identifiers':6 string com.apple.finder.SRCH" $PLIST
 
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Plists:$POSITION:_CFURLString' string 'file://$APP'" $PLIST
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Plists:$POSITION:_CFURLStringType' integer 15" $PLIST
-#/usr/libexec/PlistBuddy -c "Import 'NSToolbar Configuration Browser:TB Item Plists:$POSITION:_CFURLAliasData' alias_data" $PLIST
-/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Identifiers:$POSITION' string 'com.apple.finder.loc '" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Plists:7:_CFURLString' string 'file://$APP'" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Plists:7:_CFURLStringType' integer 15" $PLIST
+/usr/libexec/PlistBuddy -c "Add 'NSToolbar Configuration Browser:TB Item Identifiers:7' string 'com.apple.finder.loc '" $PLIST
 
 killall -HUP Finder
